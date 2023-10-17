4f607e74af06348a2a3554204f02e738e4f149bd
divin3circle <mutelema.silas21@students.dkut.ac.ke>
2023-10-17 19:20:41 +0300

    assignment4

---
+++ assignment4/README.md
@@ -0,0 +1,5 @@
+# Python grading system
+
+    - Run and enter the four marks representing the four subjects, maths, physics, geography and chemistry
+    - The program will calculate the average and print the grade alongside the average used to deduce the grade.
+    - If an invalid mark for any subject i.e marks less than 0 or greater than 100 the program assigns the grade amd average a value of -1 to indicate an error occurred during computation
--- assignment4/grading.py
+++ assignment4/grading.py
@@ -1,3 +1,5 @@
+# a python grading system for four subjects, returns -1 if invalid marks are entered
+
 user_math = input("Enter marks for math: ")
 user_physics = input("Enter marks for physics: ")
 user_geography = input("Ënter marks for geography: ")
@@ -9,6 +11,7 @@ physics = int(user_physics)
 geography = int(user_geography)
 chemistry = int(user_chemistry)
 grade = -1
+average = -1
 
 # check whether the marks for math, physics, geography and chemistry are in the required range
 if math >= 0 and math <= 100 and physics >= 0 and physics <= 100 and geography >= 0 and geography <= 100 and chemistry >= 0 and chemistry <= 100:
